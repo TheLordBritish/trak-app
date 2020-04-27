5e6b26d272ee21e7baa4145cede6b8a890057bbd
Benjamin Carter <benjamin.carter.dev@gmail.com>
2020-04-27 16:19:36 +0100

    Added error messages to the verification page with associated unit tests

---
+++ Sparky.TrakApp.ViewModel/Impl/SecureStorageService.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Sparky.TrakApp.Service;
+using Xamarin.Essentials;
+
+namespace Sparky.TrakApp.ViewModel.Impl
+{
+    public class SecureStorageService : IStorageService
+    {
+        public async Task<string> GetUsernameAsync()
+        {
+            return await SecureStorage.GetAsync("username");
+        }
+
+        public async Task SetUsernameAsync(string username)
+        {
+            await SecureStorage.SetAsync("username", username);
+        }
+
+        public async Task<string> GetAuthTokenAsync()
+        {
+            return await SecureStorage.GetAsync("auth-token");
+        }
+
+        public async Task SetAuthTokenAsync(string authToken)
+        {
+            await SecureStorage.SetAsync("auth-token", authToken);
+        }
+    }
+}

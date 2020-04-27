a4e5be499ab39bd52a7b5b322c96e73501dd65ec
Benjamin Carter <benjamin.carter.dev@gmail.com>
2020-04-27 14:56:07 +0100

    Added validation to the login page.

---
+++ Sparky.TrakApp.ViewModel/Validation/UserCredentialsValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Sparky.TrakApp.Model.Login;
+using Sparky.TrakApp.ViewModel.Resources;
+
+namespace Sparky.TrakApp.ViewModel.Validation
+{
+    public class UserCredentialsValidator : AbstractValidator<UserCredentials>
+    {
+        public UserCredentialsValidator()
+        {
+            RuleFor(r => r.Username)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty()
+                .WithMessage(Messages.LoginErrorMessageUsernameEmpty);
+
+            RuleFor(r => r.Password)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty()
+                .WithMessage(Messages.LoginErrorMessagePasswordEmpty);
+        }
+    }
+}

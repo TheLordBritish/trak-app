b902061cbf8f5cf23c0d19966e02a86b79137126
TheLordBritish <benjamin.carter.dev@gmail.com>
2020-06-06 13:50:02 +0100

    APP-4: Changed the return type for barcodes to the new object format.

---
+++ Sparky.TrakApp.Model/Games/GameBarcode.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Sparky.TrakApp.Model.Response;
+
+namespace Sparky.TrakApp.Model.Games
+{
+    public class GameBarcode : HateoasResource
+    {
+        public long Id { get; set; }
+        
+        public long GameId { get; set; }
+        
+        public long PlatformId { get; set; }
+        
+        public string Barcode { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public BarcodeType BarcodeType { get; set; }
+        
+        public long Version { get; set; }
+    }
+}

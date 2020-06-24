f2933590e98636be72db34f9f0633bee679efbed
TheLordBritish <benjamin.carter.dev@gmail.com>
2020-06-24 12:03:16 +0100

    Testing CI build.

--- Sparky.TrakApp.Android/appcenter-pre-build.sh
+++ Sparky.TrakApp.Android/appcenter-pre-build.sh
@@ -1,5 +1,8 @@
 GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/Sparky.TrakApp.Android/google-services.json
 
+echo "source directory"
+echo $APPCENTER_SOURCE_DIRECTORY
+
 # Update the google json file with the value provided by the environment variable.
 if [ -e "$GOOGLE_JSON_FILE" ]
 then

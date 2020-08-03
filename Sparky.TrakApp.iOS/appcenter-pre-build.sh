9efad5adec99f9ba9b0a2f2cb1f9a22336bf71a5
TheLordBritish <benjamin.carter.dev@gmail.com>
2020-08-03 17:32:16 +0100

    APP-16: Fix CI builds for iOS.
    Added the same appcenter prebuild script that is in the Android project to the iOS project and ensured line endings are unix format.

---
+++ Sparky.TrakApp.iOS/appcenter-pre-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/Sparky.TrakApp.Android/google-services.json
+
+# Update the google json file with the value provided by the environment variable.
+if [ -e "$GOOGLE_JSON_FILE" ]
+then
+	echo "Updating Google JSON"
+	echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
+	sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
+fi
+

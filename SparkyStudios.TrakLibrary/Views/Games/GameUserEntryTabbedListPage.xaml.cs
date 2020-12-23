e0c3056e511ef53974b2b0a035b8987a1017f1cc
bencarterdev <benjamin.carter.dev@gmail.com>
2020-12-23 16:32:54 +0000

    APP-26: Added the filter and filter results page.

---
+++ SparkyStudios.TrakLibrary/Views/Games/GameUserEntryTabbedListPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SparkyStudios.TrakLibrary.Views.Games
+{
+    public partial class GameUserEntryTabbedListPage
+    {
+        public GameUserEntryTabbedListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
